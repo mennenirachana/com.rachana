283cfd315e0122452eb978b26c63f800d85ba6a4
mennenirachana <102458807+mennenirachana@users.noreply.github.com>
2022-03-27 21:23:41 +0530

    Update Test.java
    I made some changes

--- src/test/java/pack/Test.java
+++ src/test/java/pack/Test.java
@@ -4,9 +4,10 @@ public class Test
 {
     public static void main(String[] args) 
 	{
-		int a=10;
-		int b=20;
-		System.out.println("addition is"+(a+b));
+		int a=100;
+		int b=200;
+	        
+		System.out.println("addition is"+(b-a));
 	}
 
 }
