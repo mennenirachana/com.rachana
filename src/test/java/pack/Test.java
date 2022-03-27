0b37118726f7714621b45ee7f86edb3b3e97fbf2
USER <USER@DESKTOP-ILCMAH5>
2022-03-27 20:24:17 +0530

    hi manager

--- src/test/java/pack/Test.java
+++ src/test/java/pack/Test.java
@@ -5,9 +5,9 @@ public class Test {
 	public static void main(String[] args) 
 	{
 		String a="dileep";
-		int b=56;
-		System.out.println(a);
-		System.out.println(b);
+		int b=96;
+		System.out.println("name"+a);
+		System.out.println("persentage"+b);
 
 	}
 
